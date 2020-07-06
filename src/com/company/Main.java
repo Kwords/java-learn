429bee90d103b7aaa78ef4da0f7cb7e7e1c3f467
Kwords <31701293@stu.zucc.edu.cn>
2020-07-06 21:43:19 +0800

    fix

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,5 +6,7 @@ public class Main {
 	// write your code here
         System.out.println("Hello World!");
         System.out.println("wwh-java-leran");
+        Class x=new Integer(1).getClass();
+        System.out.println(x.getClass());
     }
 }
